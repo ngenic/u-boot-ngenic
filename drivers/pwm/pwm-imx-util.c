f3749da0f5b9dfc9bb5ab7b64488ce81cc6dc856
Mattias Tegmyr <mattias.tegmyr@ngenic.se>
2018-08-14 16:50:58 +0200

    Enable PWM 5-8 for pico-mx6ul.

--- drivers/pwm/pwm-imx-util.c
+++ drivers/pwm/pwm-imx-util.c
@@ -28,7 +28,7 @@ struct pwm_regs *pwm_id_to_reg(int pwm_id)
 	case 3:
 		return (struct pwm_regs *)PWM4_BASE_ADDR;
 #endif
-#ifdef CONFIG_MX6SX
+#ifdef CONFIG_MX6SX || CONFIG_MX6UL
 	case 4:
 		return (struct pwm_regs *)PWM5_BASE_ADDR;
 	case 5:
