e705295848d0bff47bf7bf7b751a0900f7c9c3b0
Mattias Tegmyr <mattias.tegmyr@ngenic.se>
2018-08-15 14:24:55 +0200

    Revert "Enable PWM 5-8 for all MX6."
    This reverts commit 08c5dda844ae27e5b398a53c1b36d6cf460e2df8.

--- drivers/pwm/pwm-imx-util.c
+++ drivers/pwm/pwm-imx-util.c
@@ -27,6 +27,8 @@ struct pwm_regs *pwm_id_to_reg(int pwm_id)
 		return (struct pwm_regs *)PWM3_BASE_ADDR;
 	case 3:
 		return (struct pwm_regs *)PWM4_BASE_ADDR;
+#endif
+#ifdef CONFIG_MX6SX || CONFIG_MX6UL
 	case 4:
 		return (struct pwm_regs *)PWM5_BASE_ADDR;
 	case 5:
