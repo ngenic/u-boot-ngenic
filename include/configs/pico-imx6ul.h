9dc84d1c72afa32a3acdb97558ddb5e47f366266
Mattias Tegmyr <mattias.tegmyr@ngenic.se>
2018-08-27 16:59:56 +0200

    Enable loading of boot script in preboot. Move board specific settings out of u-boot to boot script.

--- include/configs/pico-imx6ul.h
+++ include/configs/pico-imx6ul.h
@@ -44,39 +44,42 @@
 		"uuid_disk=${uuid_gpt_disk};" \
 		"name=boot,size=16MiB;name=rootfs,size=0,uuid=${uuid_gpt_rootfs}\0" \
 	"setup_emmc=gpt write mmc 0 $partitions; reset;\0" \
-	"mmcargs_dev=setenv bootargs console=${console},${baudrate} " \
-		"root=PARTUUID=${uuid} rootwait ro " \
-		"fbcon=scrollback:1024k consoleblank=0 caam\0" \
 	"mmcargs=setenv bootargs console=none" \
-		"root=PARTUUID=${uuid} rootwait ro " \
-		"fbcon=scrollback:1024k consoleblank=0 caam\0" \
-	"loadimage=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"loadfdt=load mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
+		"root=PARTUUID=${uuid} rootwait ro caam\0" \
+	"boot_part=${mmcdev}:${mmcpart}"
+	"loadimage=load mmc ${boot_part} ${loadaddr} ${image}\0" \
+	"loadfdt=load mmc ${boot_part} ${fdt_addr} ${fdt_file}\0" \
+	"bootscr=boot.scr\0" \
+	"script_addr=0x81000000\0" \
+	"loadbootscr=" \
+		"fatload mmc ${boot_part} ${script_addr} ${bootscr}; || " \
+		"ext2load mmc ${boot_part} ${script_addr} ${bootscr}; || " \
+		"ext2load mmc ${boot_part} ${script_addr} boot/${bootscr};\0" \
 	"mmcboot=echo Booting from mmc ...; " \
 		"run finduuid; " \
-		"if test ${ngenic_dev_mode} = true; then " \
-			"echo Dev mode is enabled.; " \
-		    "run mmcargs_dev; " \
-		"else " \
-		    "run mmcargs; " \
-		"fi; " \
+		"run mmcargs; " \
 		"if run loadfdt; then " \
 			"bootz ${loadaddr} - ${fdt_addr}; " \
 		"else " \
 			"echo WARN: Cannot load the DT; " \
 		"fi;\0" \
-	"load_ngenic_env=" \
-	    "mmc read " CONFIG_SYS_MEMTEST_START " 0x800 0x10; " \ // Read from mmc to memory: offset block 2048 (0x800) and 16 (0x10) blocks. Using the memtest address on memory as temporary storage.
-		"env import -c " CONFIG_SYS_MEMTEST_START " 0x2000;\0" // Import env with size 16*blocksize(512) (0x2000).
 
 #define CONFIG_BOOTCOMMAND \
 	   "if mmc rescan; then " \
-	       "run load_ngenic_env; " \
-		   "run loadimage; " \
-		   "run mmcboot; " \
+		    "if run loadimage; then " \
+               	"run mmcboot; " \
+            "else " \
+		   		"echo ERROR: Cannot load kernel image.; " \
+           	"fi; " \
 	   "else " \
-	       "echo WARN: Cannot load find the mmc; " \
+	       "echo WARN: mmc rescan failed; " \
 	   "fi;" \
+
+#define CONFIG_PREBOOT \
+		"if run loadbootscr; then " \
+			"echo Found boot.scr. Executing script...; " \
+			"source ${scriptaddr}; " \
+		"fi;"
 
 #define CONFIG_SYS_LOAD_ADDR		CONFIG_LOADADDR
 #define CONFIG_SYS_HZ			1000
@@ -112,6 +115,14 @@
 
 #define CONFIG_SYS_MMC_ENV_DEV		0
 #define CONFIG_SYS_MMC_ENV_PART		0
+
+/* 
+ * From https://www.denx.de/wiki/DULG/UBootEnvVariables
+ * After reset, U-Boot will wait this number of seconds before it executes the contents of the bootcmd variable. During this time a countdown is printed, which can be interrupted by pressing any key.
+ * Set this variable to 0 boot without delay. Be careful: depending on the contents of your bootcmd variable, this can prevent you from entering interactive commands again forever!
+ * Set this variable to -1 to disable autoboot. Set this variable to -2 to boot without delay and not check for abort.
+ */
+#define CONFIG_BOOTDELAY 5
 
 /*
  * From: http://git.denx.de/?p=u-boot.git;a=blob;f=doc/README.watchdog
