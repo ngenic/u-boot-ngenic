e4c5287352c0e19a70a00335fe201e0784776bb4
Mattias Tegmyr <mattias01@gmail.com>
2019-09-19 18:33:25 +0200

    Draft for nix build.

---
+++ nix/default.nix
@@ -0,0 +1,67 @@
+let
+  pkgs = import <nixpkgs> {
+    crossSystem = {
+      config = "arm-linux-gnueabihf";
+      platform = (import <nixpkgs/lib>).systems.platforms.armv7l-hf-multiplatform;
+    };
+  };
+  inherit (pkgs) lib buildUBoot;
+
+in rec {
+  ubootBootbombMYDY6ULL = buildUBoot rec {
+    name = "u-boot-bootbomb-myd-y6ull";
+    src = ./.;
+
+    defconfig = "myd_y6ull_14x14_emmc_bootbomb_defconfig";
+
+    filesToInstall = [ "u-boot.imx" ];
+
+    postInstall = ''
+      mv $out/u-boot.imx $out/u-boot-bootbomb-myd-y6ull.imx
+    '';
+
+    extraMeta = with lib; {
+      maintainers = [ maintainers.olcai ];
+    };
+  };
+
+  ubootBootbombMYDY6ULL2 = buildUBoot rec {
+    name = "u-boot-bootbomb-myd-y6ull";
+    src = ./.;
+
+    defconfig = "myd_y6ull_14x14_emmc_defconfig";
+
+    # Start the usb mass storage gadget on boot.
+    extraConfig = ''
+      CONFIG_USE_BOOTCOMMAND=y
+      CONFIG_BOOTCOMMAND="ums 0 mmc 0"
+    '';
+
+    filesToInstall = [ "u-boot.imx" ];
+
+    postInstall = ''
+      mv $out/u-boot.imx $out/u-boot-bootbomb-myd-y6ull2.imx
+    '';
+
+    extraMeta = with lib; {
+      maintainers = [ maintainers.olcai ];
+    };
+  };
+
+  ubootPicoIMX6UL = buildUBoot rec {
+    name = "u-boot-pico-imx6ul";
+    src = ./.;
+
+    defconfig = "pico-imx6ul_defconfig";
+
+    filesToInstall = [ "u-boot.imx" ];
+
+    postInstall = ''
+      mv $out/u-boot.imx $out/u-boot-pico-imx6ul.imx
+    '';
+
+    extraMeta = with lib; {
+      maintainers = [ maintainers.olcai ];
+    };
+  };
+}
